791068c371a91f5dd8f64952508bd081bc36b453
safa-abidi <safa.abidi99@gmail.com>
2022-10-01 20:33:30 +0100

    cvs structure & model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { FirstComponent } from './components/first/first.component';
 import { CarteVisiteComponent } from './components/carte-visite/carte-visite.component';
 import { PereComponent } from './components/pere/pere.component';
 import { FilsComponent } from './components/fils/fils.component';
+import { CvComponent } from './CVs/cv/cv.component';
+import { ListComponent } from './CVs/list/list.component';
+import { ItemComponent } from './CVs/item/item.component';
+import { DetailComponent } from './CVs/detail/detail.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { FilsComponent } from './components/fils/fils.component';
     FirstComponent,
     CarteVisiteComponent,
     PereComponent,
-    FilsComponent
+    FilsComponent,
+    CvComponent,
+    ListComponent,
+    ItemComponent,
+    DetailComponent
   ],
   imports: [
     FormsModule,
